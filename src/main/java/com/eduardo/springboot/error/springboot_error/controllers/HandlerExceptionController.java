094f10593faa34b7ea7db6f945de82056402ec2e
eduRamos1620 <eduardoramosrosales16@gmail.com>
2025-02-10 01:12:48 -0600

    Se maneja error al convertir un dato no entero a entero

--- src/main/java/com/eduardo/springboot/error/springboot_error/controllers/HandlerExceptionController.java
+++ src/main/java/com/eduardo/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -1,10 +1,13 @@
 package com.eduardo.springboot.error.springboot_error.controllers;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
@@ -23,6 +26,17 @@ public class HandlerExceptionController {
         return ResponseEntity.internalServerError().body(error);
     }
 
+    @ExceptionHandler({NumberFormatException.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, Object> numberFormatException(Exception ex){
+        Map<String, Object> error = new HashMap<>();
+        error.put("date", new Date());
+        error.put("error", "Numero invalido, incluye caracteres que no son numeros");
+        error.put("message", ex.getMessage());
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return error;
+    }
+
     @ExceptionHandler(NoHandlerFoundException.class)
     public ResponseEntity<Error> notFoundEx(NoHandlerFoundException e){
         Error error = new Error();
@@ -30,7 +44,6 @@ public class HandlerExceptionController {
         error.setError("Recurso no encontrado");
         error.setMessage(e.getMessage());
         error.setStatus(HttpStatus.NOT_FOUND.value());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
-        
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);       
     }
 }
