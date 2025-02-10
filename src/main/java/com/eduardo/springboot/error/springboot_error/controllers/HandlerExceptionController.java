2f956d52f0434c271d1e9665cf5ea161a14c000c
eduRamos1620 <eduardoramosrosales16@gmail.com>
2025-02-10 00:59:48 -0600

    Personalizando Json para error 404 (Not Found) Api o Recurso no encontrado

--- src/main/java/com/eduardo/springboot/error/springboot_error/controllers/HandlerExceptionController.java
+++ src/main/java/com/eduardo/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.eduardo.springboot.error.springboot_error.models.Error;
 
@@ -20,5 +21,16 @@ public class HandlerExceptionController {
         error.setMessage(ex.getMessage());
         error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());       
         return ResponseEntity.internalServerError().body(error);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Error> notFoundEx(NoHandlerFoundException e){
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Recurso no encontrado");
+        error.setMessage(e.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+        
     }
 }
